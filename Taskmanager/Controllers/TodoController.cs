16280ca0957e60374468f8b16df5a29ac9ed5618
Patrick-Oguaju <32589922+TOSKY94@users.noreply.github.com>
2022-07-25 06:30:36 +0100

    Initial commit

---
+++ Taskmanager/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Taskmanager.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Taskmanager.Controllers
+{
+    [Route("Controller")]
+    public class TodoController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var tasks = new List<Todo>
+            {
+                new Todo {id=1, name="ASP.NET", description="ASP.NET api development", status="Not started", createDate=DateTime.Now, dueDate=DateTime.Now.AddDays(30)},
+                new Todo {id=2, name="Python Selenium", description="Selenium framework", status="In progress", createDate=DateTime.Now, dueDate=DateTime.Now.AddDays(60)},
+                new Todo {id=3, name="Django", description="Backend development", status="Completed", createDate=DateTime.Now, dueDate=DateTime.Now.AddDays(14)},
+                new Todo {id=4, name="PLSQL", description="SQL development", status="Not started", createDate=DateTime.Now, dueDate=DateTime.Now.AddDays(366)}
+            };
+
+            return Json(tasks);
+        }
+
+    }
+}
+
---
+++ Taskmanager/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Taskmanager.Models
+{
+    public class Todo
+    {
+        public int id { get; set; }
+        public string name { get; set; } = String.Empty;
+        public string description { get; set; } = String.Empty;
+        public string status { get; set; } = String.Empty;
+        public DateTime createDate { get; set; }
+        public DateTime dueDate { get; set; }
+
+    }
+}
+
